cfe92c12a325be3d20706642e1b993261a0ffc8a
esteegoel <109704190+esteegoel@users.noreply.github.com>
2023-09-25 15:17:33 -0400

    updated comments

--- solution-hw3/objects.js
+++ solution-hw3/objects.js
@@ -1,9 +1,8 @@
-// Define glazing and pack size options with their respective price changes
 const glazingOptions = {
   'Keep original': 0.00,
   'Sugar milk': 0.00,
   'Vanilla Milk': 0.50,
-  'Double Chocolate': 1.50, // Corrected capitalization
+  'Double Chocolate': 1.50, 
 };
 
 const packSizeOptions = {
@@ -13,28 +12,22 @@ const packSizeOptions = {
   '12': 10,
 };
 
-// Base price
 const basePrice = 2.49;
 
-// Function to update the price based on glazing and pack size selections
 function updatePrice() {
   const glazingSelect = document.getElementById("glazingOptions");
-  const packSizeSelect = document.getElementById("sizeOptions"); // Corrected to match your HTML
+  const packSizeSelect = document.getElementById("sizeOptions"); 
   const priceDisplay = document.getElementById("price");
 
   const selectedGlazing = glazingSelect.value;
   const selectedPackSize = packSizeSelect.value;
 
-  // Get the price changes from the options
   const glazingPriceChange = glazingOptions[selectedGlazing] || 0.00;
   const packSizeMultiplier = packSizeOptions[selectedPackSize] || 1;
 
-  // Calculate the total price by adding glazing price change and multiplying by pack size
   const totalPrice = (basePrice + glazingPriceChange) * packSizeMultiplier;
 
-  // Update the price display
   priceDisplay.textContent = `$${totalPrice.toFixed(2)}`;
 }
 
-// Initial update of the price when the page loads
 updatePrice();
